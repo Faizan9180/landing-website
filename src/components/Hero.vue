9dd8e11ab3fe13500645b3109adcd43e28a46734
Ansab Khalid <ansab.khalid444@gmail.com>
2025-05-06 01:22:14 +0500

    initial commit

---
+++ src/components/Hero.vue
@@ -0,0 +1,71 @@
+<script setup>
+import mainPhoto from "../assets/images/hero.svg";
+import { themeColor } from "../data/items";
+
+const heroHeading = "Mr. Bookman";
+const buttonSeeFeature = "See Features";
+const buttonPricing = "Pricing";
+</script>
+
+<template>
+  <div class="untree_co-hero" id="home-section">
+    <div class="container">
+      <div class="row align-items-center">
+        <div class="col-12">
+          <div class="dots"></div>
+          <div class="row align-items-center">
+            <div
+              class="col-lg-7 ml-auto order-lg-2"
+              data-aos="fade-right"
+              data-aos-delay="400"
+            >
+              <img :src="mainPhoto" alt="Image" class="img-fluid" />
+            </div>
+            <div class="col-lg-5">
+              <h1 class="heading" data-aos="fade-up" data-aos-delay="0">
+                {{ heroHeading }}
+              </h1>
+              <div class="excerpt" data-aos="fade-up" data-aos-delay="100">
+                <p>
+                  You think because you can put on a pair of pants, a shirt, and some shoes that you’re somehow above the law?
+                  <span
+                    style="
+                      background-color: rgba(64, 123, 255, 0.07);
+                      color: #407bff;
+                    "
+                    >Well,</span
+                  >
+                  let me clue you in, Joy-boy: You wouldn’t last a day on the job. You don’t have the stomach. You think this is an easy gig? All this glamour and excitement? The pressure? You have no idea the kind of stress I deal with.
+                </p>
+              </div>
+              <p data-aos="fade-up" data-aos-delay="200">
+                <a
+                  href="#features-section"
+                  class="btn btn-primary smoothscroll"
+                  style="margin-right: 5px"
+                  :style="[
+                    { background: themeColor },
+                    { borderColor: themeColor },
+                  ]"
+                  >{{ buttonSeeFeature }}</a
+                >
+                <a
+                  href="#pricing-section"
+                  class="btn btn-outline-primary smoothscroll pricing"
+                  :style="[{ color: themeColor }, { borderColor: themeColor }]"
+                  >{{ buttonPricing }}</a
+                >
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.pricing:hover {
+  color: #fff !important;
+}
+</style>
